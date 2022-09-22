a2c5a01886159f79584642e9a85b188449209a87
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-09-22 14:18:54 +0300

    first practic

---
+++ Example101/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int sqr = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равно {sqr}");
---
+++ Example102/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber == Convert.ToInt32(Math.Pow(secondNumber, 2)))
+{
+    Console.Write($"Первое число {firstNumber} является квадратом {secondNumber}");
+} else {
+    Console.Write($"Первое число {firstNumber} не является квадратом {secondNumber}");
+}
