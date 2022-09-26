b5d971211f9012bcf9306c4d79bc896c61620b82
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-09-26 21:13:28 +0300

    add practice 4

---
+++ Example428/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// 4  -> 24
+// 5  -> 120
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int limit = int.Parse (Console.ReadLine ());
+Console.WriteLine ($"Произведение чисел от 1 до {limit} равна {GetProd (limit)}");
+
+int GetProd (int limit) {
+    int prod = 1;
+    for (int i = 1; i <= limit; i++)
+        prod *= i;
+    return prod;
+}
---
+++ Example430/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] array = GetBinaryArr(8);
+
+// Console.WriteLine (array);
+
+int[] GetBinaryArr(int length) {
+    int[] arr = new int[length];
+    for(int i = 0; i < length; i++) {
+        arr[i] = new Random().Next(2);
+    }
+
+    return arr;
+}
+
+Console.WriteLine($"[{String.Join(",", array)}]");
