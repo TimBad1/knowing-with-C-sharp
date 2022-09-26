14008f97eb93636ab5f53635226efe2dd714783d
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-09-26 20:36:23 +0300

    add dr 3

--- Example319/Program.cs
+++ Example319/Program.cs
@@ -2,18 +2,35 @@
 Console.Write("Введите число ");
 int N = int.Parse(Console.ReadLine());
 
-if (N < 10000 || N > 99999) {
+if ( N < 10000 || N > 99999 ) {
     Console.WriteLine("Вы ввели не пятизначное число");
     return;
 }
 
-string str = N.ToString();
+int first = (N / 10000);
+int second = (N / 1000) % 10;
+int fourth = (N / 10) % 10;
+int fifth = N % 10;
 
-for ( int i = 0 ; i < (str.Length / 2) ; i++) {
-    if (str[i] != str[str.Length - 1 - i]) {
-        Console.WriteLine("Число не палиндром");
-        return;
-    }
+Console.WriteLine($"first = {first}");
+Console.WriteLine($"second = {second}");
+Console.WriteLine($"fourth = {fourth}");
+Console.WriteLine($"fifth = {fifth}");
+
+if (( first == fifth) && (( second == fourth ))) {
+    Console.WriteLine("Число палиндром");
+} else {
+    Console.WriteLine("Число не палиндром");
 }
 
-Console.WriteLine("Число является палиндромом");
+// string str = N.ToString();
+
+// for ( int i = 0 ; i < (str.Length / 2) ; i++) {
+//     if (str[i] != str[str.Length - 1 - i]) {
+//         Console.WriteLine("Число не палиндром");
+//         return;
+//     }
+// }
+
+
+// Console.WriteLine("Число является палиндромом");
---
+++ Example402/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int number = int.Parse (Console.ReadLine ());
+Console.WriteLine ($"В числе {number} {CountNumber(number)} цифр");
+
+int CountNumber (int number) {
+    int count = 0;
+
+    if(number < 0) {
+        number = Math.Abs(number);
+    }
+    
+    if(number == 0) {
+        return 1;
+    }
+    
+    while(number >= 1) {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
